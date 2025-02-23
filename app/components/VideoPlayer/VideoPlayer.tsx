53cd888c0b95f2c080527d199bb8ca272a6552ee
lierise <pusyak04@gmail.com>
2025-02-23 22:34:08 +0300

    Enhance VideoPlayer fullscreen handling with dynamic height

--- app/components/VideoPlayer/VideoPlayer.tsx
+++ app/components/VideoPlayer/VideoPlayer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 import Plyr from "plyr"
 import "plyr/dist/plyr.css"
 
@@ -12,18 +12,24 @@ import { BASE_PLYR_CONFIG } from "./config"
 function RegularPlayer({ src }: { src: string }) {
     const videoRef = useRef<HTMLVideoElement>(null)
     const playerRef = useRef<Plyr>(null)
+    const [isFullscreen, setIsFullscreen] = useState(false)
     useKeyboardShortcuts(videoRef)
 
     useEffect(() => {
         if (!videoRef.current) return
 
         playerRef.current = new Plyr(videoRef.current, BASE_PLYR_CONFIG)
+
+        // Слушаем события фулскрина
+        document.addEventListener("fullscreenchange", () => {
+            setIsFullscreen(!!document.fullscreenElement)
+        })
     }, [])
 
     return (
         <video
             ref={videoRef}
-            className="w-full aspect-video max-h-[calc(100vh-theme(spacing.24))]"
+            className={`w-full aspect-video ${!isFullscreen ? "max-h-[calc(100vh-theme(spacing.24))]" : ""}`}
         >
             <source
                 src={src}
@@ -35,13 +41,21 @@ function RegularPlayer({ src }: { src: string }) {
 
 function HLSPlayer({ src }: { src: string }) {
     const videoRef = useRef<HTMLVideoElement>(null)
+    const [isFullscreen, setIsFullscreen] = useState(false)
     useHls(videoRef, src)
     useKeyboardShortcuts(videoRef)
+
+    useEffect(() => {
+        // Слушаем события фулскрина
+        document.addEventListener("fullscreenchange", () => {
+            setIsFullscreen(!!document.fullscreenElement)
+        })
+    }, [])
 
     return (
         <video
             ref={videoRef}
-            className="w-full aspect-video max-h-[calc(100vh-theme(spacing.24))]"
+            className={`w-full aspect-video ${!isFullscreen ? "max-h-[calc(100vh-theme(spacing.24))]" : ""}`}
         />
     )
 }
