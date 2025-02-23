f1e404d0aca9f0bd5c5adc1de4d433362e861b10
lierise <pusyak04@gmail.com>
2025-02-23 15:31:11 +0300

    AP: Adjust remote image hostname configuration

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: "http",
-                hostname: process.env.NEXT_PUBLIC_API_URL?.replace("http://", "") || "5.35.98.122:3000"
+                hostname: process.env.NEXT_PUBLIC_API_URL?.replace("http://", "") || "5.35.98.122"
             }
         ]
     },
